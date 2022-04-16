1f5a288b310d7394324552285df1fc0b1f7ee745
Claire Denjoy <denjoy.claire@gmail.com>
2022-04-16 23:12:24 +0200

    ajout final

--- server/db.ts
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-require('dotenv').config()
-
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "tournages"
-});
